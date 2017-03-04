969cbe5fc902a48c1f9665ebcff37f6f4b3bd3dc
shashank <shrisat.p2gmail.com>
2017-03-04 18:03:48 +0000

    added apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'apache'do
+    package_name 'httpd'
+end
+service 'apache2'do
+        service_name 'httpd'
+        action [:start, :enable]
+end
+
+
---
+++ cookbooks/apache/recipes/website.rb
@@ -0,0 +1,5 @@
+file 'default www' do
+       path '/var/ww/html/index.html'
+       content 'hellow world!'
+end
+
