9dac4ac9fe27dd5d10f93c78f600ef2bc8c6955e
shashank <shrisat.p2gmail.com>
2017-03-06 06:44:24 +0000

    added progresql cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,12 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-package 'apache'do
+package 'apache2'do
     package_name 'httpd'
+    action :install
 end
 service 'apache2'do
         service_name 'httpd'
         action [:start, :enable]
 end
+
 
 
--- cookbooks/apache/recipes/website.rb
+++ cookbooks/apache/recipes/website.rb
@@ -1,5 +1,5 @@
 file 'default www' do
-       path '/var/ww/html/index.html'
+       path '/var/www/html/index.html'
        content 'hellow world!'
 end
 
